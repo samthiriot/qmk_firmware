c58c996a1331642d9bee80339df815b9c90c14e1
Samuel Thiriot <samuel.thiriot.accounts@res-ear.ch>
2022-08-29 22:39:00 +0200

    added caps words

--- keyboards/gmmk/pro/rev1/iso/keymaps/samthiriot_bepo/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/samthiriot_bepo/keymap.c
@@ -177,3 +177,27 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 }
 
 #endif
+
+// tune caps word, for bépo 
+// TODO
+/*
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+*/
