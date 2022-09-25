e8c2481d34f02140f55ebe92f79f707e797ec21e
Samuel Thiriot <samuel.thiriot.accounts@res-ear.ch>
2022-09-25 16:33:06 +0200

    led for insert status

--- keyboards/gmmk/pro/rev1/iso/keymaps/samthiriot_bepo/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/samthiriot_bepo/keymap.c
@@ -27,15 +27,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // define layers
 enum custom_user_layers {
     LAYER_BASE,
+    LAYER_LINUX,
+    LAYER_WIN,
     LAYER_FN,
     LAYER_MOUSEKEY,
     LAYER_ALT,
+    LAYER_ALTGR,
     LAYER_CTRL
 };
     // more later
     // LATEX
     // JAVA
     // HTML
+
+enum custom_keycodes {
+    COMMENT_LINE = SAFE_RANGE,
+    COMMENT_BLOCK
+};
+
+bool is_insert_active = false;
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -60,26 +70,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
     [LAYER_BASE] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INSERT,        KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        LM(LAYER_CTRL,MOD_LCTL), KC_LGUI, LM(LAYER_ALT,MOD_LALT),                            KC_SPC,                             KC_RALT, MO(LAYER_FN),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        LM(LAYER_CTRL,MOD_LCTL), KC_LGUI, LM(LAYER_ALT,MOD_LALT),                            KC_SPC,                         KC_RALT, MO(LAYER_FN),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
-
-    [LAYER_FN] = LAYOUT(
+    [LAYER_LINUX] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
+    ), 
+    [LAYER_WIN] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
+    ),
+    [LAYER_FN] = LAYOUT(
+        _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_BTN1, KC_MS_U, KC_BTN2,
         _______, _______, _______,                            _______,                            _______, _______, KC_BTN3, KC_MS_L, KC_MS_D, KC_MS_R
     ),
-
     [LAYER_ALT] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
+    ),
+    [LAYER_ALTGR] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, COMMENT_BLOCK, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
@@ -93,8 +125,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
-
-
 };
 // clang-format on
 
@@ -109,6 +139,62 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 }
 #endif
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    // deal with base layer features
+    if (record->event.pressed && get_highest_layer(layer_state) == LAYER_BASE) {
+
+        switch (keycode) {
+            case KC_INSERT: 
+                // change the state of insert        
+                is_insert_active = !is_insert_active;
+                return true;
+                break;
+        }
+
+    }
+
+
+    // manage switching OS layers
+    if (record->event.pressed && IS_LAYER_ON_STATE(layer_state, LAYER_FN)) {
+        switch (keycode) {
+        // click linux
+        case KC_F6: 
+            if (IS_LAYER_ON_STATE(layer_state, LAYER_LINUX)) {
+                layer_off(LAYER_LINUX);
+            } else {
+                layer_on(LAYER_LINUX);
+                layer_off(LAYER_WIN);
+            }
+            return false;
+            break;
+        // click windows
+        case KC_F7: 
+            if (IS_LAYER_ON_STATE(layer_state, LAYER_WIN)) {
+                layer_off(LAYER_WIN);
+            } else {
+                layer_off(LAYER_LINUX);
+                layer_on(LAYER_WIN);
+            }
+            return false;
+            break;
+        }
+    }
+
+    // not working yet...
+    switch (keycode) {
+    case COMMENT_BLOCK:
+        if (record->event.pressed) {
+            // when keycode QMKBEST is pressed
+            SEND_STRING("/**\\");
+        } else {
+            // when keycode QMKBEST is released
+        }
+        break;
+    }
+    return true;
+};
+
 #ifdef RGB_MATRIX_ENABLE
 
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
@@ -116,6 +202,11 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     static uint8_t  current_value   = 0;
     static uint8_t  left_side_leds[8] = {68, 71, 74, 77, 81, 84, 88, 92};
     static uint8_t  right_side_leds[8] = {69, 72, 75, 78, 82, 85, 89, 93};
+
+    // show the status of insert
+    if (is_insert_active) {
+        rgb_matrix_set_color(LED_PRT, RGB_WHITE);
+    }
 
     // when caps lock is activated
     if (host_keyboard_led_state().caps_lock || is_caps_word_on()) {
@@ -136,51 +227,62 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         }
     }
 
+    // if linux mode is ON
+    if (IS_LAYER_ON_STATE(layer_state, LAYER_LINUX)) {
+        rgb_matrix_set_color(LED_F6,     RGB_WHITE);
+    }
+    if (IS_LAYER_ON_STATE(layer_state, LAYER_WIN)) {
+        rgb_matrix_set_color(LED_F7,     RGB_WHITE);
+    }
+
     // when FN is pushed, highlight in red the mapped keys
     switch(get_highest_layer(layer_state)){  // special handling per layer
        case LAYER_BASE:  //layer one
          break;
        case LAYER_FN:
          // highlight flashing 
-         rgb_matrix_set_color(LED_BSPC, RGB_YELLOW);
+         rgb_matrix_set_color(LED_BSPC,  RGB_ORANGE);
          // highlight directional keys
-         rgb_matrix_set_color(LED_UP,    RGB_CYAN);
-         rgb_matrix_set_color(LED_DOWN,  RGB_CYAN);
-         rgb_matrix_set_color(LED_LEFT,  RGB_CYAN);
-         rgb_matrix_set_color(LED_RIGHT, RGB_CYAN);
+         rgb_matrix_set_color(LED_UP,    RGB_WHITE);
+         rgb_matrix_set_color(LED_DOWN,  RGB_WHITE);
+         rgb_matrix_set_color(LED_LEFT,  RGB_WHITE);
+         rgb_matrix_set_color(LED_RIGHT, RGB_WHITE);
          // highlight mouse keys
-         rgb_matrix_set_color(LED_RSFT,  RGB_OFFBLUE);
-         rgb_matrix_set_color(LED_END,   RGB_OFFBLUE);
-         rgb_matrix_set_color(KC_BTN3,   RGB_OFFBLUE);
+         rgb_matrix_set_color(LED_RSFT,  RGB_SPRINGGREEN);
+         rgb_matrix_set_color(LED_END,   RGB_SPRINGGREEN);
+         rgb_matrix_set_color(KC_BTN3,   RGB_SPRINGGREEN);
          break;
        case LAYER_ALT:
          rgb_matrix_set_color(LED_TAB,   RGB_WHITE);
          rgb_matrix_set_color(LED_F4,    RGB_WHITE);
 
          // highlight directional keys
-         rgb_matrix_set_color(LED_UP,    RGB_CYAN);
-         rgb_matrix_set_color(LED_DOWN,  RGB_CYAN);
-         rgb_matrix_set_color(LED_LEFT,  RGB_CYAN);
-         rgb_matrix_set_color(LED_RIGHT, RGB_CYAN);
+         rgb_matrix_set_color(LED_UP,    RGB_SPRINGGREEN);
+         rgb_matrix_set_color(LED_DOWN,  RGB_SPRINGGREEN);
+         rgb_matrix_set_color(LED_LEFT,  RGB_SPRINGGREEN);
+         rgb_matrix_set_color(LED_RIGHT, RGB_SPRINGGREEN);
          break;
        case LAYER_CTRL:
          // highlight ctrl C, ctrl V, ctrl Z...
-         rgb_matrix_set_color(LED_Z,     RGB_CYAN);
-         rgb_matrix_set_color(LED_X,     RGB_CYAN);
-         rgb_matrix_set_color(LED_C,     RGB_CYAN);
-         rgb_matrix_set_color(LED_Q,     RGB_CYAN);
+         rgb_matrix_set_color(LED_Z,     RGB_WHITE);
+         rgb_matrix_set_color(LED_X,     RGB_WHITE);
+         rgb_matrix_set_color(LED_C,     RGB_WHITE);
+         rgb_matrix_set_color(LED_Q,     RGB_WHITE);
 
          rgb_matrix_set_color(LED_S,     RGB_WHITE);
 
-         rgb_matrix_set_color(LED_W,     RGB_OFFBLUE);
-         rgb_matrix_set_color(LED_Y,     RGB_OFFBLUE);
+         rgb_matrix_set_color(LED_W,     RGB_WHITE);
+         rgb_matrix_set_color(LED_Y,     RGB_WHITE);
         
          // highlight directional keys
-         rgb_matrix_set_color(LED_UP,    RGB_CYAN);
-         rgb_matrix_set_color(LED_DOWN,  RGB_CYAN);
-         rgb_matrix_set_color(LED_LEFT,  RGB_CYAN);
-         rgb_matrix_set_color(LED_RIGHT, RGB_CYAN);
+         rgb_matrix_set_color(LED_UP,    RGB_SPRINGGREEN);
+         rgb_matrix_set_color(LED_DOWN,  RGB_SPRINGGREEN);
+         rgb_matrix_set_color(LED_LEFT,  RGB_SPRINGGREEN);
+         rgb_matrix_set_color(LED_RIGHT, RGB_SPRINGGREEN);
          break;
+       //case LAYER_LINUX:
+       //  rgb_matrix_set_color(LED_BSPC,  RGB_YELLOW);
+       // break;
        default:
          break;
        break;
